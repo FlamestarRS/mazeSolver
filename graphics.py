3b96e14c4462750f93c899c005b346738f02f330
FlamestarRS <FlamestarRS@gmail.com>
2025-12-18 10:56:07 -0500

    draw_line method, Point and Line class

--- graphics.py
+++ graphics.py
@@ -1,3 +1,4 @@
+from __future__ import annotations
 from tkinter import Tk, BOTH, Canvas
 
 
@@ -5,9 +6,9 @@ class Window:
     def __init__(self, width, height):
         self.__root = Tk()
         self.__root.title("Maze Solver")
-        self.canvas = Canvas(self.__root, height=height, width=width)
-        self.canvas.pack(fill=BOTH, expand=1)
-        self.running = False
+        self.__canvas = Canvas(self.__root, height=height, width=width)
+        self.__canvas.pack(fill=BOTH, expand=1)
+        self.__running = False
         self.__root.protocol("WM_DELETE_WINDOW", self.close)
     
     def redraw(self):
@@ -15,9 +16,29 @@ class Window:
         self.__root.update()
 
     def wait_for_close(self):
-        self.running = True
-        while self.running:
+        self.__running = True
+        while self.__running:
             self.redraw()
 
     def close(self):
-        self.running = False
+        self.__running = False
+
+    def draw_line(self, line: Line, color):
+        line.draw(self.__canvas, color)
+
+class Point:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+class Line:
+    def __init__(self, p1: Point, p2: Point):
+        self.p1 = p1
+        self.p2 = p2
+
+    def draw(self, canvas: Canvas, color):
+        canvas.create_line(
+            self.p1.x, self.p1.y, 
+            self.p2.x, self.p2.y, 
+            fill=color, width=2
+            )
--- main.py
+++ main.py
@@ -1,9 +1,11 @@
-from graphics import Window
+from graphics import *
 
 def main():
     win = Window(800, 600)
+    test_line = Line(Point(400, 400), Point(300, 300))
+    test_color = "red"
+    win.draw_line(test_line, test_color)
     win.wait_for_close()
-
 
 if __name__ == "__main__":
     main()
